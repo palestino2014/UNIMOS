3a91d3b34af949ce09ecb9eca82614eeaaa958d4
Jefferson Mendes <acrifagia@yahoo.com.br>
2019-05-17 20:19:29 -0300

    minor fixes

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var $checky = $("input[type=checkbox].checky");
+    $checky.click(function() {
+    //calculateTotal();
+    if ($(this).is(":checked")) {
+        calculateTotal();
+
+    } else {
+        var index = $(this).closest("tr").attr("data-index");
+        calculateTotal();
+    }
+});
+
+function calculateTotal() {
+//if($(this).is(":checked")) {
+    var total = 0;
+
+    $checky.filter(":checked").each(function() {
+        total = parseFloat(total) + parseFloat($(this).val());
+    });
+    $("#total").text(total);
+    
+}
